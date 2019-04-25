253e9a80cc874bf824c490f170eb5a513b681470
jinlong <jinlong@riverrun.cn>
2019-04-25 23:36:49 +0800

    注释：单向链表是否有环判断

--- src/main/java/linkedlist/demo/DemoSingleLinkedList2.java
+++ src/main/java/linkedlist/demo/DemoSingleLinkedList2.java
@@ -287,6 +287,9 @@ public class DemoSingleLinkedList2 implements ISingleLinkedList {
 
     /**
      * 判断单向链表中是否存在环
+     * 快慢指针法： 两个指针同时从链表头移动 慢指针每次移动1步； 快指针每次移动2步；
+     * 快慢指针如果重合说明一定有环 证明：
+     * https://blog.csdn.net/sinat_35261315/article/details/79205157
      * @param head
      * @return null：没有环；非null：快慢指针的相遇点
      */
@@ -374,7 +377,6 @@ public class DemoSingleLinkedList2 implements ISingleLinkedList {
     }
 
     //TODO
-
     /**
      * 合并两个LinkedList
      * @param headA
